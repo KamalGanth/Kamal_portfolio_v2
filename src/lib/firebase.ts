ac8560a446256c17eadf2555281641a15fd43899
Kamalganth S <136149957+KamalGanth@users.noreply.github.com>
2025-08-19 11:57:39 +0530

    api Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAxPjuwufrjvmzAPmihWgIs446nTwPo0T8",
+  apiKey: "api",
   authDomain: "kamalganth-portfolio.firebaseapp.com",
   projectId: "kamalganth-portfolio",
   storageBucket: "kamalganth-portfolio.firebasestorage.app",
